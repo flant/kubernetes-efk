92e0c23c0cb17bbd3ed010a084016d82beea20af
Andrey Klimentyev <andrey.klimentyev@flant.com>
2017-08-19 01:51:21 +0300

    fixed path names

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ kctl() {
     kubectl --namespace "$NAMESPACE" "$@"
 }
 
-kctl apply -f manifests/elasticsearch/es-master
+kctl apply -f manifests/es-master
 
 printf "Waiting for es-master"
 until kctl get deploy es-master > /dev/null 2>&1; do sleep 1; printf "."; done
@@ -18,8 +18,8 @@ until kctl get svc elasticsearch-discovery > /dev/null 2>&1; do sleep 1; printf 
 until kctl get svc elasticsearch > /dev/null 2>&1; do sleep 1; printf "."; done
 echo "Done!"
 
-kctl apply -f manifests/elasticsearch/es-client
-kctl apply -f manifests/elasticsearch/es-data
+kctl apply -f manifests/es-client
+kctl apply -f manifests/es-data
 
 printf "Waiting for es-client and es-data"
 until kctl get deploy es-client > /dev/null 2>&1; do sleep 1; printf "."; done
@@ -29,7 +29,7 @@ until kctl get svc elasticsearch-data > /dev/null 2>&1; do sleep 1; printf "."; 
 echo "Done!"
 
 echo "Deploying es-curator"
-kctl apply -f manifests/elasticsearch/es-curator
+kctl apply -f manifests/es-curator
 
 echo "Deploying fluentd"
 kctl apply -f manifests/fluentd
--- teardown.sh
+++ teardown.sh
@@ -8,9 +8,9 @@ kctl() {
     kubectl --namespace "$NAMESPACE" "$@"
 }
 
-kctl delete -f manifests/elasticsearch/es-master
-kctl delete -f manifests/elasticsearch/es-client
-kctl delete -f manifests/elasticsearch/es-data
-kctl delete -f manifests/elasticsearch/es-curator
+kctl delete -f manifests/es-master
+kctl delete -f manifests/es-client
+kctl delete -f manifests/es-data
+kctl delete -f manifests/es-curator
 kctl delete -f manifests/fluentd
 kctl delete -f manifests/kibana
